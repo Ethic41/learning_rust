3a51e88b7c063623b720d664c59ff10c50454dc3
ethic41 <dahirmuhammad3@gmail.com>
2024-07-07 13:44:04 +0100

    feat: Add formatted print examples
    This commit adds examples of formatted print statements in the `main` function. It demonstrates various ways to format and print values using placeholders and formatting options.
    
    The code includes examples of printing integers in different number bases, justifying and padding numbers, and using named arguments for formatting.

---
+++ src/1-hello-world/formatted-print.rs
@@ -0,0 +1,29 @@
+/* 
+ * =====[Bismillahirrahmanirrahim]=====
+ * -*- coding: utf-8 -*-
+ * @Date    : 2024-07-07 11:24:51
+ * @Author  : Your Name (you@example.org)
+ * @Link    : link
+ * @Version : 1.0.0
+ */
+
+
+fn main(){
+    println!("{} days", 31);
+
+    println!("{0}, this is {1}, this is {0}", "Alice", "Bob");
+
+    println!("{subject} {verb} {object}", object="the lazy dog", subject="the quick brown fox", verb="jumps over");
+    println!("Base 10:               {}",   69420); // 69420
+    println!("Base 2 (binary):       {:b}", 69420); // 10000111100101100
+    println!("Base 8 (octal):        {:o}", 69420); // 207454
+    println!("Base 16 (hexadecimal): {:x}", 69420); // 10f2c
+
+    // justify
+    println!("{number:>5}", number=12);
+
+    // padding
+    println!("{number:0<8}", number=18);
+    println!("{number:0>5}", number=185);
+
+}
