d4f297d942a6a5a9b6b4b86954f65c4eac727fc1
ethic41 <dahirmuhammad3@gmail.com>
2024-07-07 13:44:33 +0100

    feat: Add main.rs file with hello world program
    This commit adds a new file `main.rs` with a simple "Hello World" program in Rust. The program prints the message "hello world!" to the console.
    
    The purpose of this commit is to provide a starting point for a Rust project and demonstrate the basic structure of a Rust program.
    
    Note: The commit message has been modified to adhere to the established commit message conventions.

---
+++ main.rs
@@ -0,0 +1,12 @@
+/* =====[Bismillahirrahmanirrahim]=====
+ * -*- coding: utf-8 -*-
+ * @Date    : 2024-07-07 12:20:57
+ * @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+ * @Link    : link
+ * @Version : 1.0.0
+ */
+
+
+fn main(){
+    println!("hello world!");
+}
