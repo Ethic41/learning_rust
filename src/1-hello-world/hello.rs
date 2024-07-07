3dbdc05c558d2b0aa1ff8d75ca282728114c350f
ethic41 <dahirmuhammad3@gmail.com>
2024-07-07 13:44:17 +0100

    feat: Add hello world example in Rust
    This commit adds a new file `hello.rs` in the `1-hello-world` directory. The file contains a simple "Hello World" program written in Rust. It prints the message "Hello World!" and "I'm a Rustafarean" to the console.
    
    The purpose of this commit is to provide a basic example of a Rust program for beginners to understand the syntax and structure of a Rust program.
    
    The code is self-contained and does not have any dependencies.
    
    Note: The commit message has been modified to adhere to the established commit message conventions.

---
+++ src/1-hello-world/hello.rs
@@ -0,0 +1,13 @@
+/* =====[Bismillahirrahmanirrahim]=====
+ * -*- coding: utf-8 -*-
+ * @Date    : 2024-07-07 11:08:15
+ * @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+ * @Link    : link
+ * @Version : 1.0.0
+ */
+
+
+fn main(){
+    println!("Hello World!");
+    println!("I'm a Rustafarean");
+}
