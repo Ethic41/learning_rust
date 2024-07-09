346356dbc20ce902de089263e977acc0934b266b
ethic41 <dahirmuhammad3@gmail.com>
2024-07-09 05:09:53 +0100

    feat: Add code for primitive data types in Rust
    This commit adds a new file `primitives.rs` in the `2-primitives` directory. The file contains code examples for working with primitive data types in Rust, including booleans, floats, integers, and bitwise operations.
    
    The purpose of this commit is to provide a reference for beginners to understand the different primitive data types in Rust and how to use them in their programs.

---
+++ src/2-primitives/primitives.rs
@@ -0,0 +1,59 @@
+/* =====[Bismillahirrahmanirrahim]=====
+ * -*- coding: utf-8 -*-
+ * @Date    : 2024-07-08 19:55:46
+ * @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+ * @Link    : link
+ * @Version : 1.0.0
+ */
+
+
+fn main(){
+    // variables can be type annotated
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0; // regular annotation
+    let an_integer = 5i32; // suffix annotation
+    let another_integer: i32 = 256; // regular annotation
+
+    // or a default will be used
+    let default_float = 3.0; // `f64`
+    let default_integer = 7; // `i32`
+
+    // A type can also be inferred from context
+    let mut inferred_type = 12; // type i64 is inferred from another line
+    inferred_type = 4294967296i64;
+
+    // a mutable's variable value can be changed
+    let mut mutable: i32 = 12; // mutable i32
+    mutable = 21;
+
+    // but it's type can be changed
+    // mutable = true
+
+    // literals and operators
+    // Integer addition
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    // Integer subtraction
+    println!("1 - 2 = {}", 1i32 - 2);
+    // TODO ^ Try changing `1i32` to `1u32` to see why the type is important
+
+    // Scientific notation
+    println!("1e4 is {}, -2.5e-3 is {}", 1e4, -2.5e-3);
+
+    // Short-circuiting boolean logic
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    // Use underscores to improve readability!
+    println!("One million is written as {}", 1_000_000u32);
+
+}
