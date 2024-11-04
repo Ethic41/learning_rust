8d0f55665c4feb79f704b2f34e2749be8b5e49e6
ethic41 <dahirmuhammad3@gmail.com>
2024-11-04 08:49:21 +0100

    feat: Add constant and function examples to custom-types.rs

--- src/3-custom-types/custom-types.rs
+++ src/3-custom-types/custom-types.rs
@@ -9,6 +9,14 @@
 
 #![allow(dead_code)]
 
+static LANGUAGE: &str = "Rust";
+const THRESHOLD: i32 = 10;
+
+fn is_big(n: i32) -> bool {
+    // Access constant in some function
+    n > THRESHOLD
+}
+
 
 use crate::List::*;
 
@@ -216,6 +224,17 @@ fn main() {
     // Show the final state of the list
     println!("linked list has length: {}", list.len());
     println!("{}", list.stringify());
+
+    let n = 16;
+
+    // Access constant in the main thread
+    println!("This is {}", LANGUAGE);
+    println!("The threshold is {}", THRESHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else { "small" });
+
+    // Error! Cannot modify a `const`.
+    // THRESHOLD = 5;
+    
 }
 
 // Define a structure named `WebEvent`
